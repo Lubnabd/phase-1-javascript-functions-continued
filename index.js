291a4ea9b567b696efd9d2d4840f310a2f81f029
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-10-05 14:00:58 +0300

    not yet completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+//QUESTION 1 
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+//QUESTION 2 
+
+function mondayWork(place = 'go to the office') {
+    return `This Monday, I will ${place}.`;
+  }
+
+//QUESTION 3 
+
+function wrapAdjective (inner = )
